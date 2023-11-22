733fd4eacc3dffa7d5d6b163ad1f85336a9ff3a0
fatiihvarol <fatihvrl38@gmail.com>
2023-11-22 21:28:16 +0300

    initial commit

---
+++ src/test/java/com/example/healt/joy/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healt.joy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
